2a124a93b753c934a5441a0ac6cde05aee949ee7
Last Studio Ever <laststudioever@gmail.com>
2022-07-13 01:44:37 +0500

    edited description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,17 +11,17 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'My name is Usama Ahmed Siddiquie. This site is a database of my procedural artwork.' },
+      { hid: 'description', name: 'description', content: 'Film and visual arts.' },
       { name: 'format-detection', content: 'telephone=no' },
 
       // Open Graph
-      { property:'og:url', content: 'https://colatz.com/' },
+      { property:'og:url', content: 'https://laststudioever.com/' },
       { property:'og:type', content: 'website' },
-      { property:'og:title', content: 'Colatz | Database for Usama Ahmed' },
-      { property:'og:description', content: 'My name is Usama Ahmed Siddiquie. This site is a database of my procedural artwork.' },
-      { property:'og:image', content: 'https://colatz.com/link-share-banner.png' },
-      { property:'og:image:alt', content: 'https://colatz.com/link-share-banner.png' },
-      { property:'og:image:secure_url', content: 'https://colatz.com/link-share-banner.png' },
+      { property:'og:title', content: 'Last Studio Ever' },
+      { property:'og:description', content: 'Film and visual arts.' },
+      { property:'og:image', content: 'https://laststudioever.com/link-share-banner.png' },
+      { property:'og:image:alt', content: 'https://laststudioever.com/link-share-banner.png' },
+      { property:'og:image:secure_url', content: 'https://laststudioever.com/link-share-banner.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/icon-round-32.ico' }
